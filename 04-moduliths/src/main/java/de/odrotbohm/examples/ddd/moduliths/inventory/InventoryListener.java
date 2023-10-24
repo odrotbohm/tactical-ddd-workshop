f41f3a06b76bf27bcd21509751313ca9a8787312
Oliver Drotbohm <odrotbohm@vmware.com>
2023-10-25 00:31:53 +0200

    Upgrade to Spring Modulith 1.1 RC1.

--- 04-moduliths/src/main/java/de/odrotbohm/examples/ddd/moduliths/inventory/InventoryListener.java
+++ 04-moduliths/src/main/java/de/odrotbohm/examples/ddd/moduliths/inventory/InventoryListener.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.jmolecules.event.annotation.DomainEventHandler;
-import org.springframework.modulith.ApplicationModuleListener;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
